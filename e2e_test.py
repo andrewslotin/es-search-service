a2093a4adc78a01660f676bab9698706412ca8c4
Andrew Slotin <andrew.slotin@gmail.com>
2019-09-08 13:52:11 +0200

    Change the e2e test to include filtering, sorting and pagination

--- e2e_test.py
+++ e2e_test.py
@@ -8,13 +8,50 @@ SERVICE_URL = "localhost:8080"
 DATA = ('{"index": {"_index": "products", "_type": "product"}}\n'
         '{"title": "AirMax", "brand": "Nike", "price": 1000, "stock": 10}\n'
         '{"index": {"_index": "products", "_type": "product"}}\n'
-        '{"title": "SuperStar", "brand": "Adidas", "price": 999, "stock": 5}\n')
+        '{"title": "Pegasus Shield", "brand": "Nike", "price": 1500, "stock": 12}\n'
+        '{"index": {"_index": "products", "_type": "product"}}\n'
+        '{"title": "Pegasus Shield", "brand": "Nike", "price": 2000, "stock": 20}\n'
+        '{"index": {"_index": "products", "_type": "product"}}\n'
+        '{"title": "Zoom", "brand": "Nike", "price": 2000, "stock": 1}\n'
+        '{"index": {"_index": "products", "_type": "product"}}\n'
+        '{"title": "SuperStar", "brand": "Adidas", "price": 999, "stock": 5}\n'
+        
+)
+MAPPINGS = """
+{
+  "properties": {
+    "title": {
+      "type": "text",
+      "fielddata": true
+    },
+    "brand": {
+      "type": "text",
+      "fielddata": true
+    },
+    "price": {
+      "type": "long"
+    },
+    "stock": {
+      "type": "long"
+    }
+  }
+}
+"""
 
-def seed(data):
+def seed(data, mappings):
     print("Uploading seed data")
     
     conn = http.client.HTTPConnection(ELASTICSEARCH_URL)
-    conn.request("POST", "/_bulk", data, {"Content-Type": "application/x-ndjson"})
+    conn.request("POST", "/_bulk", data, headers={"Content-Type": "application/x-ndjson"})
+
+    resp = conn.getresponse()
+    if resp.status >= 400:
+        raise BaseException("failed to seed cluster: " + str(resp.read()))
+
+    conn.close()
+
+    conn = http.client.HTTPConnection(ELASTICSEARCH_URL)
+    conn.request("PUT", "/products/_mapping", mappings, headers={"Content-Type": "application/json"})
 
     resp = conn.getresponse()
     if resp.status >= 400:
@@ -35,7 +72,7 @@ def clear():
     conn.close()
 
 def query_search_api(query):
-    path = "/v1/products?q=" + query
+    path = "/v1/products?" + query
     conn = http.client.HTTPConnection(SERVICE_URL)
     conn.request("GET", path, headers={
         "Authorization": "Basic " + b64encode(b"user1:password2").decode("ascii")
@@ -69,27 +106,51 @@ def test(name, query, expected):
 
 
 failed = 0
-seed(DATA)
+seed(DATA, MAPPINGS)
 time.sleep(2) # give ES a chance to index
 
 try:
     if not test(
-        "Find 'Nike'",
-        "Nike",
-        '{"status":"success","results":[{"title":"AirMax","brand":"Nike","price":1000,"stock":10}]}'
+        "Find all 'Nike' with price 1500",
+        "q=Nike&filter=price:1500",
+        """
+        {
+          "status": "success",
+          "results": [
+            {
+              "title": "Pegasus Shield",
+              "brand": "Nike",
+              "price": 1500,
+              "stock": 12
+            }
+          ]
+        }
+        """
     ):
         failed += 1
 
     if not test(
-        "Find 'SuperStar'",
-        "SuperStar",
-        '{"status":"success","results":[{"title":"SuperStar","brand":"Adidas","price":999,"stock":5}]}'
+        "Find all 'Nike Pegasus Shield' sorted by title and price (expencieve first), return 2nd page of results with one result per page",
+        "q=Nike&sort=title:asc&sort=price:desc&filter=title:Pegasus+Shield&from=1&size=1",
+        """
+        {
+          "status": "success",
+          "results": [
+            {
+              "title": "Pegasus Shield",
+              "brand": "Nike",
+              "price": 1500,
+              "stock": 12
+            }
+          ]
+        }
+        """
     ):
         failed += 1
 
     if not test(
         "Don't find 'Puma'",
-        "Puma",
+        "q=Puma",
         '{"status":"success","results":[]}'
     ):
         failed += 1
